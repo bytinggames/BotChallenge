010e41fbf354735ee4d169b265c312038f74870d
bytinggames <bytinggames@gmail.com>
2020-06-06 16:55:46 +0200

    instruction to create and get Program.cs running

---
+++ BotChallenge/CarRace/_Copy_Program.cs
@@ -0,0 +1,41 @@
+﻿/* please copy this class and rename it to Program.cs, then in the new file, write one / in front of this first line to enable the Program class
+// like so: //* please copy this class....
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BotChallenge;
+using BotChallenge.CarRace;
+
+namespace CarRace
+{
+    class Program
+    {
+        const bool visible = true;
+
+        static void Main(string[] args)
+        {
+            // determine which classes race against each other
+            Type[] bots = new Type[]
+            {
+
+                typeof(Human),
+                //typeof(HumanWASD),
+                //typeof(EmptyBot),
+            };
+
+            RunManagerTournament runManager = new RunManagerTournament(typeof(EnvCarRace), bots, bots.Length, 1, visible, OutputMode.ResultOfEveryIteration, new System.Random().Next());
+
+            runManager.Loop();
+
+            if (visible)
+                Console.ReadLine();
+        }
+    }
+}
+
+
+//*/
